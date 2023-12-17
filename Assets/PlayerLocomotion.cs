f7f17a35bc11ab3c864ce9de5212b37150fc6190
xiaorenping <85027396@qq.com>
2023-12-17 14:06:29 +0800

    03-InputSystem简单使用二  ##创建脚本连接InputAction输入操作调用人物模型

---
+++ Assets/PlayerLocomotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    PlayerControls inputActions;
+    new Rigidbody rigidbody;
+    Vector2 movementInput;
+
+    [Header("Stats")]
+    [SerializeField]
+    float movementSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = new Vector3(movementInput.x * movementSpeed, 0, movementInput.y * movementSpeed);
+        rigidbody.velocity = vector;
+    }
+
+    private void OnEnable()
+    {
+        if(inputActions == null)
+        {
+            inputActions = new PlayerControls();
+            inputActions.PlayerMovement.MovementAction.performed += o => movementInput = o.ReadValue<Vector2>();
+        }
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+}
