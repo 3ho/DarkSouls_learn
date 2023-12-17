08c05faba6cd5e21645c2344fb2d4f49ec7abb95
xiaorenping <85027396@qq.com>
2023-12-17 17:05:49 +0800

    04-角色移动旋转 ##InputSystem输入与Player分离

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+    public float moveAmount;
+    public float mouseX;
+    public float mouseY;
+
+    PlayerControls inputActions;
+    Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        if (inputActions == null)
+        {
+            inputActions = new PlayerControls();
+            inputActions.PlayerMovement.MovementAction.performed += outputActions => movementInput = outputActions.ReadValue<Vector2>();
+        }
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    public void TickInput(float delta)
+    {
+        MoveInput(delta);
+    }
+
+    public void MoveInput(float delta)
+    {
+        horizontal = movementInput.x;
+        vertical = movementInput.y;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+    }
+}
--- Assets/PlayerLocomotion.cs
+++ Assets/PlayerLocomotion.cs
@@ -4,39 +4,67 @@ using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
-    PlayerControls inputActions;
+    InputHandler inputHandler;
+    Vector3 moveDirection;
+
     new Rigidbody rigidbody;
     Vector2 movementInput;
+    Transform cameraObject;
+
+    [HideInInspector]
+    public Transform myTransform;
 
     [Header("Stats")]
     [SerializeField]
     float movementSpeed = 5;
+    [SerializeField]
+    float rotationSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        inputHandler = GetComponent<InputHandler>();
+        cameraObject = Camera.main.transform;
+        myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 vector = new Vector3(movementInput.x * movementSpeed, 0, movementInput.y * movementSpeed);
-        rigidbody.velocity = vector;
+        float delta = Time.deltaTime;
+        inputHandler.TickInput(delta);
+
+        moveDirection = cameraObject.forward * inputHandler.vertical;
+        moveDirection += cameraObject.right * inputHandler.horizontal;
+        moveDirection.Normalize();
+
+        float speed = movementSpeed;
+        moveDirection *= speed;
+
+        //moveDirection.y = 0;
+        Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, new Vector3(0, 0, 0));
+        rigidbody.velocity = projectedVelocity;
+
+        HandleRotation(delta);
     }
 
-    private void OnEnable()
-    {
-        if(inputActions == null)
-        {
-            inputActions = new PlayerControls();
-            inputActions.PlayerMovement.MovementAction.performed += o => movementInput = o.ReadValue<Vector2>();
-        }
-        inputActions.Enable();
-    }
 
-    private void OnDisable()
+    private void HandleRotation(float delta)
     {
-        inputActions.Disable();
+        Vector3 targetDir = Vector3.zero;
+
+        targetDir = cameraObject.forward * inputHandler.vertical;
+        targetDir += cameraObject.right * inputHandler.horizontal;
+        targetDir.Normalize();
+        targetDir.y = 0;
+
+        if (targetDir == Vector3.zero)
+            targetDir = myTransform.forward;
+
+        Quaternion tr = Quaternion.LookRotation(targetDir);
+        Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rotationSpeed * delta);
+
+        myTransform.rotation = targetRotation;
     }
 }
