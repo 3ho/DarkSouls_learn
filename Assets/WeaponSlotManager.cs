8845a8e54992419627122492aeb16e01abf2f477
xiaorenping <85027396@qq.com>
2023-12-19 20:46:21 +0800

    17-角色持有武器二

---
+++ Assets/WeaponSlotManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlotManager : MonoBehaviour
+{
+    WeaponHodlerSlot leftHandSlot;
+    WeaponHodlerSlot rightHandSlot;
+
+    private void Awake()
+    {
+        WeaponHodlerSlot[] weaponHodlerSlots = GetComponentsInChildren<WeaponHodlerSlot>();
+        foreach(WeaponHodlerSlot weaponSlot in weaponHodlerSlots)
+        {
+            if(weaponSlot.isLeftHandSlot)
+            {
+                leftHandSlot = weaponSlot;
+            }else if(weaponSlot.isRightHandSlot)
+            {
+                rightHandSlot = weaponSlot;
+            }
+        }
+    }
+
+    public void LoadWeaponOnSlot(WeaponItem weaponItem, bool isLeft)
+    {
+        if(isLeft)
+        {
+            leftHandSlot.LoadWeaponModel(weaponItem);
+        }
+        else
+        {
+            rightHandSlot.LoadWeaponModel(weaponItem);
+        }
+    }
+}
