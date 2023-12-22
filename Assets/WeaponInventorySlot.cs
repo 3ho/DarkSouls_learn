26c3cabc6ae1b5fe3014afff28e86fb690d39e17
xiaorenping <85027396@qq.com>
2023-12-22 20:21:37 +0800

    45-装备栏武器切换二

--- Assets/WeaponInventorySlot.cs
+++ Assets/WeaponInventorySlot.cs
@@ -5,9 +5,18 @@ using UnityEngine.UI;
 
 public class WeaponInventorySlot : MonoBehaviour
 {
+    PlayerInventory playerInventory;
+    WeaponSlotManager weaponSlotManager;
+    UIManager uiManager;
     public Image icon;
     public WeaponItem item;
 
+    private void Awake()
+    {
+        playerInventory = FindObjectOfType<PlayerInventory>();
+        weaponSlotManager = FindObjectOfType<WeaponSlotManager>();
+        uiManager = FindObjectOfType<UIManager>();
+    }
     public void AddItem(WeaponItem newItem)
     {
         item = newItem;
@@ -22,5 +31,45 @@ public class WeaponInventorySlot : MonoBehaviour
         icon.sprite = null;
         icon.enabled = false;
         gameObject.SetActive(false);
+    }
+
+    public void EquipThisItem()
+    {
+        if(uiManager.rightHandSlot01Select)
+        {
+            playerInventory.weaponsInventory.Add(playerInventory.weaponInRightHandSlots[0]);
+            playerInventory.weaponInRightHandSlots[0] = item;
+            playerInventory.weaponsInventory.Remove(item);
+        }else if (uiManager.rightHandSlot02Select)
+        {
+            playerInventory.weaponsInventory.Add(playerInventory.weaponInRightHandSlots[1]);
+            playerInventory.weaponInRightHandSlots[1] = item;
+            playerInventory.weaponsInventory.Remove(item);
+        }
+        else if (uiManager.leftHandSlot01Select)
+        {
+            playerInventory.weaponsInventory.Add(playerInventory.weaponInLeftHandSlots[0]);
+            playerInventory.weaponInLeftHandSlots[0] = item;
+            playerInventory.weaponsInventory.Remove(item);
+        }
+        else if (uiManager.leftHandSlot02Select)
+        {
+            playerInventory.weaponsInventory.Add(playerInventory.weaponInLeftHandSlots[1]);
+            playerInventory.weaponInLeftHandSlots[1] = item;
+            playerInventory.weaponsInventory.Remove(item);
+        }
+        else
+        {
+            return;
+        }
+
+        playerInventory.rightWeapon = playerInventory.weaponInRightHandSlots[playerInventory.currentRightWeaponIndex];
+        playerInventory.leftWeapon = playerInventory.weaponInLeftHandSlots[playerInventory.currentLeftWeaponIndex];
+
+        weaponSlotManager.LoadWeaponOnSlot(playerInventory.rightWeapon, false);
+        weaponSlotManager.LoadWeaponOnSlot(playerInventory.leftWeapon, true);
+
+        uiManager.equipmentWindowUI.LoadWeaponsOnEquipmentScreen(playerInventory);
+        uiManager.ResetAllSelectedSlots();
     }
 }
